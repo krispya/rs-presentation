c818678ab433164825e2c24c26e34cb064cdc33c
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-11-18 00:33:37 -0500

    🎉 finish/spawn-two

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,35 @@
 import { Canvas } from '@react-three/fiber';
+import { useActions } from 'koota/react';
+import { useEffect } from 'react';
+import { actions } from './actions';
 import { BulletRenderer } from './components/bullet-renderer';
 import { EnemyRenderer } from './components/enemy-renderer';
-import { PlayerRenderer } from './components/player-view';
+import { PlayerRenderer } from './components/player-renderer';
 
 export function App() {
 	return (
 		<Canvas camera={{ fov: 50, position: [0, 0, 50] }}>
+			<Startup />
+
 			<PlayerRenderer />
 			<EnemyRenderer />
 			<BulletRenderer />
 		</Canvas>
 	);
+}
+
+function Startup() {
+	const { spawnPlayer, spawnEnemy } = useActions(actions);
+
+	useEffect(() => {
+		const player = spawnPlayer();
+		const enemySpawnInterval = setInterval(() => spawnEnemy(), 1000);
+
+		return () => {
+			player.destroy();
+			clearInterval(enemySpawnInterval);
+		};
+	}, [spawnPlayer, spawnEnemy]);
+
+	return null;
 }
