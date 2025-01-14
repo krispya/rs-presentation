970a13d47378bac9d71bd32d95f4e3db9875da8f
Kris Baumgartner <kjbaumgartner@gmail.com>
2025-01-13 22:47:19 -0500

    🔧 Triplex config
    Signed-off-by: Kris Baumgartner <kjbaumgartner@gmail.com>

---
+++ .triplex/provider.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { WorldProvider } from 'koota/react';
+import { world } from '../src/world';
+
+export default function Provider({ children }: { children?: React.ReactNode }) {
+	return <WorldProvider world={world}>{children}</WorldProvider>;
+}
